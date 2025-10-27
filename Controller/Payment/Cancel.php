1166c96fdc4851fe837f96febe54ab3068dcbbf8
chinmes-patra <chinmes@tryspeed.com>
2025-10-27 14:10:17 +0530

    Updated Cancelation process to update order status to cancel

--- Controller/Payment/Cancel.php
+++ Controller/Payment/Cancel.php
@@ -2,30 +2,42 @@
 
 namespace Tryspeed\BitcoinPayment\Controller\Payment;
 
-class Cancel extends \Magento\Framework\App\Action\Action
-{
-    protected $checkoutHelper;
-    
-    public function __construct(
-        \Magento\Framework\App\Action\Context $context,
-        \Magento\Checkout\Helper\Data $checkoutHelper,
-    ) {
-        parent::__construct($context);
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Sales\Model\OrderFactory;
+use Magento\Checkout\Model\Session as CheckoutSession;
 
-        $this->checkoutHelper = $checkoutHelper;
+class Cancel extends Action
+{
+    protected $checkoutSession;
+    protected $orderFactory;
+
+    public function __construct(
+        Context $context,
+        CheckoutSession $checkoutSession,
+        OrderFactory $orderFactory
+    ) {
+        $this->checkoutSession = $checkoutSession;
+        $this->orderFactory = $orderFactory;
+        parent::__construct($context);
     }
 
-    /**
-     * Payment cancel function
-     *
-     * @return void
-     */
     public function execute()
     {
-        $session = $this->checkoutHelper->getCheckout();
-        $lastRealOrderId = $session->getLastRealOrderId();
-        $session->restoreQuote();
-        $session->setLastRealOrderId($lastRealOrderId);
-        return $this->_redirect('checkout/cart');
+        try {
+            $order = $this->checkoutSession->getLastRealOrder();
+            if ($order && $order->getId() && $order->getState() !== \Magento\Sales\Model\Order::STATE_CANCELED) {
+                $order->cancel();
+                $order->addStatusHistoryComment(__('Customer canceled the payment.'));
+                $order->save();
+            }
+
+            $this->checkoutSession->restoreQuote();
+            $this->messageManager->addNoticeMessage(__('You have canceled the payment.'));
+        } catch (\Exception $e) {
+            $this->messageManager->addErrorMessage(__('Unable to cancel the order: %1', $e->getMessage()));
+        }
+
+        return $this->_redirect('checkout/onepage/failure');
     }
 }
